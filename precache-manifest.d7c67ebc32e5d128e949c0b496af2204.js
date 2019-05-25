b9b368ade6dc58abe41b42270a6ffae059b9c31d
Nick <nickkossolapov@gmail.com>
2019-05-25 16:31:25 +0200

    Updates

--- precache-manifest.d7c67ebc32e5d128e949c0b496af2204.js
+++ precache-manifest.d7c67ebc32e5d128e949c0b496af2204.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7e07f133b3e3be7de4c71cb2b964eb51",
+    "revision": "b38b04c6d4dd4c301a8caae04b95d27d",
     "url": "/in-bed/index.html"
   },
   {
-    "revision": "420fe8bdbfe9f86a11a7",
-    "url": "/in-bed/static/css/main.c800a2ca.chunk.css"
+    "revision": "3e89570340ea13c9d0c7",
+    "url": "/in-bed/static/css/main.0cf4bfbb.chunk.css"
   },
   {
     "revision": "f92196b3a569796b82c2",
     "url": "/in-bed/static/js/2.7fbf3b9e.chunk.js"
   },
   {
-    "revision": "420fe8bdbfe9f86a11a7",
-    "url": "/in-bed/static/js/main.e7295c49.chunk.js"
+    "revision": "3e89570340ea13c9d0c7",
+    "url": "/in-bed/static/js/main.678aee9f.chunk.js"
   },
   {
     "revision": "46b420e428ec8e15c5a1",
