c12678333c02f5a7ab56c2fd009b3c65d07717fe
Nick <nickkossolapov@gmail.com>
2019-05-25 15:15:01 +0200

    Add lightbox to galleries

--- src/components/imageGallery.jsx
+++ src/components/imageGallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './imageGallery.css';
@@ -8,26 +8,50 @@ export function imageGallery(name, albumLength, header) {
     [...Array(albumLength).keys()].map(number => `/${name}/${number}.jpg`)
   )];
 
-
   return () => {
     return (
       <main>
         <header>
-          <h1>{header}</h1>
+          <h1>
+            <Link to="/works">{header}</Link>
+          </h1>
         </header>
-
-        <section class="masonry">
-          {
-            images.map(image => {
-              return <img src={image} alt=""/>
-            })
-          }
-        </section>
-
+        <Images images={images}/>
         <footer>
           <Link to="/works">back</Link>
         </footer>
       </main>
     )
   }
+}
+
+function Images(props) {
+  let [lightboxImage, setLightboxImage] = useState(null);
+  let {images} = props;
+
+  return (
+    <>
+      {
+        lightboxImage && <Lightbox image={lightboxImage} closeLightbox={() => setLightboxImage(null)}/>
+      }
+      <section className="masonry">
+
+        {
+          images.map(image => {
+            return <img src={image} alt="" onClick={() => setLightboxImage(image)} key={image}/>
+          })
+        }
+      </section>
+    </>
+
+  );
+}
+
+function Lightbox(props) {
+  let { image, closeLightbox } = props;
+  return (
+    <section className="lightbox" onClick={closeLightbox}>
+      <img src={'/large' + image} alt="" onClick={closeLightbox}/>
+    </section>
+  )
 }
