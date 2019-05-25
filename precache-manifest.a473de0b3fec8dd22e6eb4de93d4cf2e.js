00f2f3722291e1db688138098916b3fe260770b3
Nick <nickkossolapov@gmail.com>
2019-05-25 15:20:35 +0200

    Updates

---
+++ precache-manifest.a473de0b3fec8dd22e6eb4de93d4cf2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed73ac39f554559ad24a8c0a3239e951",
+    "url": "/in-bed/index.html"
+  },
+  {
+    "revision": "7f6be0d6af4a2c8a0fef",
+    "url": "/in-bed/static/css/main.d40074fa.chunk.css"
+  },
+  {
+    "revision": "f92196b3a569796b82c2",
+    "url": "/in-bed/static/js/2.7fbf3b9e.chunk.js"
+  },
+  {
+    "revision": "7f6be0d6af4a2c8a0fef",
+    "url": "/in-bed/static/js/main.56cd41a7.chunk.js"
+  },
+  {
+    "revision": "46b420e428ec8e15c5a1",
+    "url": "/in-bed/static/js/runtime~main.f87ed27c.js"
+  },
+  {
+    "revision": "1e5549a999a2c5f2462b3e73f6549096",
+    "url": "/in-bed/static/media/about.1e5549a9.jpg"
+  },
+  {
+    "revision": "2c940f81cd724a0ad5ba97b4c83ed337",
+    "url": "/in-bed/static/media/home.2c940f81.jpg"
+  }
+]);
