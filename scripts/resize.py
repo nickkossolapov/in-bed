a72d9d8e46c2e6ba1c3ac6c180aeae5027128b82
Nick <nickkossolapov@gmail.com>
2019-05-25 11:02:19 +0200

    Add image assets

--- scripts/resize.py
+++ scripts/resize.py
@@ -7,6 +7,7 @@ img_dirs = ['begging', 'fred', 'gustavo', 'sweet']
 
 for img_dir in img_dirs:
     size = 300, 10000
+    size_large = 900, 10000
 
     for ind, infile in enumerate(glob.glob(f"./originals/{img_dir}/*.jpg")):
         file, ext = os.path.splitext(infile)
@@ -15,3 +16,9 @@ for img_dir in img_dirs:
         filename = f"./assets/{img_dir}/{ind}.jpg"
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         im.save(filename, "JPEG")
+
+        im_large = Image.open(infile)
+        im_large.thumbnail(size_large)
+        filename = f"./assets/large/{img_dir}/{ind}.jpg"
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        im_large.save(filename, "JPEG")
